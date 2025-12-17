006103082eebe70d557283d492f453b1e822bd3f
Jyothsna-31862 <your-email@example.com>
2025-12-17 19:46:29 +0530

    Adding microservices

---
+++ Hostel-Service/src/test/java/com/hostelservice/Hostel/Service/HostelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostelservice.Hostel.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
